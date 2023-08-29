847a0d17a8d8d903af200d47aca1d3a6fc04f666
Arjay Onan <archessonan.1@gmail.com>
2023-08-29 09:10:30 +0800

    edited admin registration path

--- src/admin/Login.tsx
+++ src/admin/Login.tsx
@@ -74,7 +74,7 @@ export default function AdminLogin() {
         </div>
       </form>
 
-      <h1 className='text-xl text-white'>Create an admin account <Link to="/admin_register" className='text-sky-500 underline'>here.</Link></h1>
+      <h1 className='text-xl text-white'>Create an admin account <Link to="/admin_registration" className='text-sky-500 underline'>here.</Link></h1>
     </div>
   )
 }
